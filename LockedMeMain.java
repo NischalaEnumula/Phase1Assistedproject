8120fe56d8158b9e753de15f12fa32c3b086921a
NischalaEnumula <97800625+NischalaEnumula@users.noreply.github.com>
2022-01-16 17:19:20 +0530

    Add files via upload

---
+++ LockedMeMain.java
@@ -0,0 +1,14 @@
+package phase1project1;
+
+public class LockedMeMain {
+public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		MenuOptions.printWelcomeScreen("Locker", "dimpu");
+		
+		HandleOption.handleWelcomeScreenInput();
+	}
+}
+
